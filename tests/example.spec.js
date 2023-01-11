7cd0db9295c7d5096e9e97cc0194ac59f815f1af
hoshi-R <85061940+hoshi-R@users.noreply.github.com>
2023-01-11 11:09:57 +0900

    ７
    ７

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('homepage has title and links to intro page', async ({ page }) => {
   await page.goto('https://www.cman.jp/network/support/go_access.cgi');
+  
   await page.screenshot({ path: 'screenshot.png' });
   await page.close();
 });
